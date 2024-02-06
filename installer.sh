3e3a673a36bdc33c2d1147acfc1a21b7783b65d5
JustAGuyLinux <drew@drewgriffin.com>
2024-02-05 20:30:17 -0500

    testing phase

--- installer.sh
+++ installer.sh
@@ -8,13 +8,21 @@ bash ~/debian-hyprland/resources/02-waypackages
 xdg-user-dirs-update
 mkdir ~/Screenshots/
 
-# Thunar
-sudo apt install -y thunar thunar-archive-plugin thunar-volman file-roller
-
 # Fonts and icons for now
 sudo apt install -y fonts-recommended fonts-ubuntu fonts-font-awesome fonts-terminus
 bash ~/debian-hyprland/resources/nerdfonts.sh
 
+# rofi wayland
+bash ~/debian-hyprland/resources/03-rofi-wayland
+
+# Thunar
+sudo apt install -y thunar thunar-archive-plugin thunar-volman file-roller
+
+# SDDM
+sudo apt-get install --no-install-recommends -y sddm
+
+# nwg-look
+bash ~/debian-hyprland/resources/04-nwg-look
 
 sudo apt autoremove
 
